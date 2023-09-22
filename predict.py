f96c4c56412ff4fff709e6f945e6e3182d6e1cdb
Rafael Marasca Martins <rafaelmarascamartins@hotmail.com>
2023-09-22 17:46:25 -0300

    update classifier

--- predict.py
+++ predict.py
@@ -32,25 +32,10 @@ X_train = scaler.fit_transform(X_train)
 
 X_test = scaler.transform(X_test)
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(6, 1)),
-  tf.keras.layers.Dense(32, activation='relu'),
-  tf.keras.layers.Dense(64, activation='relu'),
-  tf.keras.layers.Dense(32, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation='softmax')
-])
 
-opt = tf.keras.optimizers.SGD(learning_rate=0.01)
-model.compile(optimizer=opt,
-              loss='sparse_categorical_crossentropy',
-              metrics=['categorical_accuracy'])
+model = tf.keras.models.load_model('modelo.keras')
 
-
-model.fit(X_train, y_train, epochs=500, batch_size= 64, validation_split=0.2)
 model.evaluate(X_test,  y_test)
-
-model.save('modelo.keras')
 
 y_pred = model.predict(X_test)
 y_pred = y_pred.argmax(1)
